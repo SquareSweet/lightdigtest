368073d73fd26a829c2a86a1056b77af1f0e0fc4
Svetlana Antoshkina <svetlana120595@gmail.com>
2024-03-08 07:12:07 +0300

    Spring template

---
+++ src/main/java/me/sqsw/lightdigtest/LightdigtestApplication.java
@@ -0,0 +1,13 @@
+package me.sqsw.lightdigtest;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class LightdigtestApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(LightdigtestApplication.class, args);
+	}
+
+}
