02f2b41059534c94eee5673a3160c013b0bac690
Svetlana Antoshkina <svetlana120595@gmail.com>
2024-03-10 12:34:31 +0300

    Request data model

---
+++ src/main/java/me/sqsw/lightdigtest/model/Request.java
@@ -0,0 +1,34 @@
+package me.sqsw.lightdigtest.model;
+
+import jakarta.persistence.*;
+import lombok.Getter;
+import lombok.Setter;
+
+import java.time.LocalDateTime;
+
+@Entity
+@Getter
+@Setter
+@Table(name = "requests")
+public class Request {
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+
+    @ManyToOne
+    @JoinColumn(name = "user_id", referencedColumnName = "id")
+    private User user;
+
+    private String title;
+    private String text;
+    private RequestState state;
+
+    @Column(name = "phone_number")
+    private String phoneNumber;
+
+    @Column(name = "created_on")
+    private LocalDateTime createdOn;
+
+    @Column(name = "sent_on")
+    private LocalDateTime sentOn;
+}
